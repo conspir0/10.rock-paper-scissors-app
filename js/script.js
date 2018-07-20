5691727cd60707ec28d5aa136c1bb0088ad0b5a8
M Michalczyk <pan.michalczyk@gmail.com>
2018-07-20 11:58:27 +0200

    Add Start game, Result. App is ready

--- js/script.js
+++ js/script.js
@@ -1,17 +1,60 @@
 'use strict';
 
-var rockBtn = document.querySelector('#player-rock'),
+var startBtn = document.querySelector('#btn-start-game'),
+    rockBtn = document.querySelector('#player-rock'),
     paperBtn = document.querySelector('#player-paper'),
     scissorsBtn = document.querySelector('#player-scissors'),
+    panelGame = document.querySelector('.panel-game'),
+    buttons = document.querySelector('#buttons'),
+    playerName = document.querySelector('#player-name'),
+    round = document.querySelector('#round'),
+    result = document.querySelector('#result'),
     output = document.querySelector('#output'),
+    gameRounds,
+    name,
     playerPoints = 0,
     computerPoints = 0;
 
+// Game
+panelGame.style.display = "none";
+output.style.display = "none";
+buttons.style.display = "none";
+
+// Start game
+var startGame = function() {
+    name = window.prompt('What is your name?');
+    if(!isNaN(name)) {
+        output.style.display = "block";
+        output.innerHTML = "Don't be shy :-)";
+        console.log(output.innerHTML);
+    } else {
+        window.alert('Good luck ' + name + " ! Let's start");
+        gameRounds = window.prompt('How many games would you like to play?');
+        if(gameRounds == '' || gameRounds <= 0 || isNaN(gameRounds)) {
+            panelGame.style.display = "none";
+            buttons.style.display = "none";
+            output.style.display = "block";
+            output.innerHTML = "Wrong value, please write again.";
+        } else {
+            round.innerHTML = gameRounds;
+            panelGame.style.display = "block";
+            buttons.style.display = "flex";
+            output.style.display = "block";
+            playerPoints = 0;
+            computerPoints = 0;
+            playerName.innerHTML = name + ' : Computer';
+            result.innerHTML = "0 : 0";
+            output.innerHTML = '';
+        }
+    }
+}
+
+startBtn.addEventListener('click', function() {startGame();})
 
 // Player move ---
-rockBtn.addEventListener('click', function() {result('rock')});
-paperBtn.addEventListener('click', function() {result('paper')});
-scissorsBtn.addEventListener('click', function() {result('scissors')});
+rockBtn.addEventListener('click', function() {resultGame('rock')});
+paperBtn.addEventListener('click', function() {resultGame('paper')});
+scissorsBtn.addEventListener('click', function() {resultGame('scissors')});
 
 // Computer move ---
 var computerMove = function() {
@@ -25,27 +68,28 @@ var computerMove = function() {
     }
 }
 
-// Who is the winner
+// Who win one tour
 var whoIsTheWinner = function(player, computer) {
     if(player == 'rock' && computer == 'scissors' || player == 'paper' && computer == 'rock' || player == 'scissors' && computer == 'paper') {
-        return 'you';
+        return name;
     } else if(player == 'rock' && computer == 'paper' || player == 'paper' && computer == 'scissors' || player == 'scissors' && computer == 'rock') {
-        return 'computer';
+        return 'Computer';
     } else {
-        return 'noboy';
+        return 'Noboy';
     }
 }
 
 // Points
 var points = function(winner) {
-    if(winner == 'you') {
+    if(winner == name) {
        return playerPoints++;
-       } else if(winner == 'computer') {
+       } else if(winner == 'Computer') {
         return computerPoints++
     }   
 }
 
-var result = function(player) {
+
+var resultGame = function(player) {
     
     // Computer move
     var computer = computerMove();
@@ -56,6 +100,22 @@ var result = function(player) {
     // Points
     var pointsForGame = points(winner);
     
-    // Output
-    output.innerHTML = 'Player: ' + player + ' Computer: ' + computer + '<br>The winner is ' + winner;
+    // Result
+    var resultContent = function() {
+        result.innerHTML = playerPoints + ' : ' + computerPoints;
+        output.innerHTML = 'THE END<br><br><strong>' + winnerGame + ' is the winner!</strong>';
+    }
+        var winnerGame;
+        if(gameRounds == playerPoints) {
+            winnerGame = name;
+            resultContent();
+            buttons.style.display = 'none';
+        } else if(gameRounds == computerPoints) {
+            winnerGame = "Computer";
+            resultContent();
+            buttons.style.display = 'none';
+        } else {
+            result.innerHTML = playerPoints + ' : ' + computerPoints;
+            output.innerHTML = name + ': <strong>' + player + '</strong> Computer: <strong>' + computer + '</strong> . ' + winner + ' won.<br>' + output.innerHTML;
+        }
 }
